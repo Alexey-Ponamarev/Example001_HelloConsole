ddba9c007607e4b86becd3db7897d99878f4736c
Alexey-Ponamarev <146445792+Alexey-Ponamarev@users.noreply.github.com>
2023-10-11 22:43:40 +0300

    random

--- Example004_Dive/Program.cs
+++ Example004_Dive/Program.cs
@@ -1,5 +1,8 @@
-﻿double num_1=5;
-double num_2=3;
-double num_3=2;
-double res=num_1+num_2/num_3;
+﻿int num_1=new Random().Next(1,10);
+int num_2=new Random().Next(1,10);
+int num_3=new Random().Next(1,10);
+int res=num_1+num_2*num_3;
+Console.WriteLine(num_1);
+Console.WriteLine(num_2);
+Console.WriteLine(num_3);
 Console.WriteLine(res);
